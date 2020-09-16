30a60d0dd5fdff4eac3e45a2b728f6ef37b0a087
Mijin <mjmjmj9840@gmail.com>
2020-09-16 22:00:22 +0900

    Prevent Sleep in Heroku Server

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 //jshint esversion:6
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
-const flash = require("connect-flash");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const flash = require('connect-flash');
+const http = require('http');
 
 const app = express();
 
+// Prevent Sleep in Heroku Server
+setInterval(function () {
+  http.get('https://mjmjmj9840-blog.herokuapp.com');
+}, 600000);
+
 // App Set
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
@@ -36,7 +42,7 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
   useFindAndModify: true,
 });
-mongoose.set("useCreateIndex", true);
+mongoose.set('useCreateIndex', true);
 
 // Post Shcema
 const postsSchema = new mongoose.Schema({
@@ -45,7 +51,7 @@ const postsSchema = new mongoose.Schema({
   comments: Array,
 });
 
-const Post = new mongoose.model("Post", postsSchema);
+const Post = new mongoose.model('Post', postsSchema);
 
 // User Schema
 const userSchema = new mongoose.Schema({
@@ -58,7 +64,7 @@ const userSchema = new mongoose.Schema({
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-const User = new mongoose.model("User", userSchema);
+const User = new mongoose.model('User', userSchema);
 
 // Passport Configure Strategy
 passport.use(User.createStrategy());
@@ -78,8 +84,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://peaceful-coast-67831.herokuapp.com/auth/goolge/blog",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+      callbackURL: 'https://mjmjmj9840-blog.herokuapp.com/auth/goolge/blog',
+      userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
     },
     function (accessToken, refreshToken, profile, cb) {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -90,56 +96,56 @@ passport.use(
 );
 
 // Google Authentication
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile"] }));
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 
 app.get(
-  "/auth/google/blog",
-  passport.authenticate("google", { failureRedirect: "/login" }),
+  '/auth/google/blog',
+  passport.authenticate('google', { failureRedirect: '/login' }),
   function (req, res) {
-    res.redirect("/");
+    res.redirect('/');
   }
 );
 
-app.get("/logout", function (req, res) {
+app.get('/logout', function (req, res) {
   req.logout();
-  res.redirect("/");
+  res.redirect('/');
 });
 
 // Home Route
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   Post.find({}, function (err, foundPosts) {
     if (!err) {
       if (req.isAuthenticated()) {
         // already login
-        res.render("home", { headerName: "header-login", posts: foundPosts });
+        res.render('home', { headerName: 'header-login', posts: foundPosts });
       } else {
-        res.render("home", { headerName: "header", posts: foundPosts });
+        res.render('home', { headerName: 'header', posts: foundPosts });
       }
     }
   });
 });
 
 // Contact Route
-app.get("/contact", function (req, res) {
+app.get('/contact', function (req, res) {
   if (req.isAuthenticated()) {
     // already login
-    res.render("contact", { headerName: "header-login" });
+    res.render('contact', { headerName: 'header-login' });
   } else {
-    res.render("contact", { headerName: "header" });
+    res.render('contact', { headerName: 'header' });
   }
 });
 
 // Compose Route
-app.get("/compose", function (req, res) {
+app.get('/compose', function (req, res) {
   if (req.isAuthenticated()) {
     // already login
-    res.render("compose", { headerName: "header-login" });
+    res.render('compose', { headerName: 'header-login' });
   } else {
-    res.render("compose", { headerName: "header" });
+    res.render('compose', { headerName: 'header' });
   }
 });
 
-app.post("/compose", function (req, res) {
+app.post('/compose', function (req, res) {
   const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
@@ -148,29 +154,29 @@ app.post("/compose", function (req, res) {
 
   post.save(function (err) {
     if (!err) {
-      res.redirect("/");
+      res.redirect('/');
     }
   });
 });
 
 // Post Route
-app.get("/posts/:postID", function (req, res) {
+app.get('/posts/:postID', function (req, res) {
   const requestedID = req.params.postID;
 
   Post.findOne({ _id: requestedID }, function (err, foundPost) {
     if (!err) {
       if (!foundPost) {
         // Post가 없는 경우
-        res.redirect("/");
+        res.redirect('/');
       } else {
         if (req.isAuthenticated()) {
-          res.render("post", {
-            headerName: "header-login",
+          res.render('post', {
+            headerName: 'header-login',
             title: foundPost.title,
             contents: foundPost.content,
           });
         } else {
-          res.redirect("/login");
+          res.redirect('/login');
         }
       }
     }
@@ -178,22 +184,22 @@ app.get("/posts/:postID", function (req, res) {
 });
 
 // Login Route
-app.get("/login", function (req, res) {
+app.get('/login', function (req, res) {
   const fmsg = req.flash();
-  var msg = "LOGIN"; // default message
+  var msg = 'LOGIN'; // default message
   if (fmsg.error) {
     msg = fmsg.error[0]; // error message
   }
 
   if (req.isAuthenticated()) {
     // already login
-    res.render("login", { headerName: "header-login", loginMessage: msg });
+    res.render('login', { headerName: 'header-login', loginMessage: msg });
   } else {
-    res.render("login", { headerName: "header", loginMessage: msg });
+    res.render('login', { headerName: 'header', loginMessage: msg });
   }
 });
 
-app.post("/login", function (req, res) {
+app.post('/login', function (req, res) {
   const user = new User({
     username: req.body.username,
     password: req.body.password,
@@ -202,13 +208,13 @@ app.post("/login", function (req, res) {
   req.login(user, function (err) {
     if (err) {
       console.log(err);
-      res.redirect("/login");
+      res.redirect('/login');
     } else {
-      passport.authenticate("local", { failureRedirect: "/login", failureFlash: true })(
+      passport.authenticate('local', { failureRedirect: '/login', failureFlash: true })(
         req,
         res,
         function () {
-          res.redirect("/");
+          res.redirect('/');
         }
       );
     }
@@ -216,23 +222,23 @@ app.post("/login", function (req, res) {
 });
 
 // Sign Up Route
-app.get("/signUp", function (req, res) {
+app.get('/signUp', function (req, res) {
   if (req.isAuthenticated()) {
     // already login
-    res.render("signUp", { headerName: "header-login" });
+    res.render('signUp', { headerName: 'header-login' });
   } else {
-    res.render("signUp", { headerName: "header" });
+    res.render('signUp', { headerName: 'header' });
   }
 });
 
-app.post("/signUp", function (req, res) {
+app.post('/signUp', function (req, res) {
   User.register({ username: req.body.username }, req.body.password, function (err, user) {
     if (err) {
       console.log(err);
-      res.redirect("/signUp");
+      res.redirect('/signUp');
     } else {
-      passport.authenticate("local")(req, res, function () {
-        res.redirect("/");
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/');
       });
     }
   });
@@ -240,10 +246,10 @@ app.post("/signUp", function (req, res) {
 
 // Starting Server
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port == null || port == '') {
   port = 3000;
 }
 
 app.listen(port, function () {
-  console.log("Server started successfully.");
+  console.log('Server started successfully.');
 });
